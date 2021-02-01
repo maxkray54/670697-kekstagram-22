7ffc75264d19c335f11abbc3d9e46eafcabb785d
maxkray54 <maxkray99@gmail.com>
2021-02-01 22:18:18 +0300

    Утилитарные функции 2.0

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 //Основу брал из источника: https://learn.javascript.ru/task/random-int-min-max
 
+// Функция проверки макс меньше либо равен мин
+const isMaxLessThanMin = (min, max) => (max <= min);
+
+// Функция проверки мин и макс меньше нуля
+const isMinMaxLessThanZero = (min, max) => (min < 0) || (max < 0);
+
 // Функция вывода рандоного числа в заданном промежутке
 const randomInteger = function (min, max) {
   const rand = min + Math.random() * (max + 1 - min);
-  if (min < 0, max < 0, max <= min) {
+  if (isMinMaxLessThanZero(min, max) || isMaxLessThanMin(min, max)) {
     window.console.log('invalid value');
   } else {
     return Math.floor(rand);
   }
 }
-window.console.log(randomInteger(10, 1));
+window.console.log(randomInteger(1, 10));
 
 //Функция для проверки максимальной длины строки. получаем либо true либо false
 const countingSymbols = function (text, maxValue) {
