fa2916a4a5cb3c82bd9fafe37ecb872dbbdaeffb
maxkray54 <maxkray99@gmail.com>
2021-02-15 16:23:10 +0300

    Много постов с комментариями

--- js/main.js
+++ js/main.js
@@ -21,4 +21,124 @@ window.console.log(randomInteger(1, 10));
 const countingSymbols = function (text, maxValue) {
   return text.length <= maxValue;
 }
-window.console.log(countingSymbols('Привет JS', 9))
+window.console.log(countingSymbols('Привет JS', 9));
+
+//Функция для создания объекта c данными пользователя
+
+const creatingUser = function (id, manyComments, randomManyDescriptions) {
+  let user = {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: randomManyDescriptions,
+    likes: randomInteger(15, 200),
+    comment: manyComments,
+  }
+
+  return user;
+}
+
+//Массив с описаниями к постам
+
+let description = [
+  'Круто отдохнули',
+  'Невидомый закат',
+  'Полнолуние',
+  'Кто гулять?',
+  'Паррррриж',
+  'Ловлю ритм',
+  'Подарите машину, которая не ломается',
+  'Какое кино посмотреть',
+  'На охоте',
+  'Купил айфон',
+];
+
+const getRandomDescription = function () {
+  const descriptionUser = randomInteger(0, 9);
+  const randomDescription = description[descriptionUser];
+  return randomDescription;
+}
+window.console.log(getRandomDescription());
+
+//Массив с именами и функция которая выдает рандомное имя
+
+let name = [
+  'Миша',
+  'Аня',
+  'Даник',
+  'Катя',
+  'Юля',
+  'Света',
+  'Костя',
+  'Соня',
+  'Витя',
+  'Женя',
+  'Вася',
+  'Агния',
+  'Настя',
+  'Юра',
+  'Влад',
+  'Саша',
+  'Гена',
+  'Ира',
+  'Таня',
+  'Игорь',
+  'Илья',
+  'Оля',
+  'Вика',
+  'Олег',
+  'Вадим',
+];
+
+//Функция для рандомного имени из массива через индекс
+
+const getRandomName = function () {
+  const nameUser = randomInteger(0, 24);
+  const randomName = name[nameUser];
+  return randomName;
+}
+window.console.log(getRandomName());
+
+
+const creatingComment = function (randomManyNames, id) {
+  let comment = {
+    id: id,
+    avatar: `img/avatar-${randomInteger(1, 6)}.svg`,
+    message: 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    name: randomManyNames,
+  }
+
+  return comment;
+}
+
+window.console.log(creatingComment());
+
+const uniqNumbers = [];
+const getRandUniqNumbers = function () {
+  let isUniq = false;
+  while (isUniq == false) {
+    const rand = randomInteger(1, 1000);
+    if (!uniqNumbers.includes(rand)) {
+      uniqNumbers.push(rand);
+      isUniq = true;
+    }
+  }
+  return uniqNumbers[uniqNumbers.length - 1];
+}
+
+//Функция для создания массива c пользователя и массивов комментариев
+const getManyUser = function () {
+  const users = [];
+  for (let i = 0; i < 25; i++) {
+    const comments = [];
+    const countComments = randomInteger(3, 15);
+    const randomManyDescriptions = getRandomDescription();
+    for (let g = 0; g < countComments; g++) {
+      const commentsId = getRandUniqNumbers();
+      const randomManyNames = getRandomName();
+      comments.push(creatingComment(randomManyNames, commentsId));
+    }
+    users.push(creatingUser(i + 1, comments, randomManyDescriptions));
+  }
+  window.console.log(users);
+}
+getManyUser();
