e97e045bd2d3a0527d2a90ac84ac205c1219686d
maxkray54 <maxkray99@gmail.com>
2021-03-16 22:07:26 +0300

    Перламутровые пуговицы (часть 1)

---
+++ js/filter-posts.js
@@ -0,0 +1,44 @@
+/* global _:readonly */
+import { drawUsersPictures } from './pictures.js';
+
+const BIDE = 500;
+const RANDOM_PICTURES_QUANTITY = 10;
+
+const imgFilter = document.querySelector('.img-filters');
+const imgFilterForm = document.querySelector('.img-filters__form');
+
+const setDefaultPosts = (array) => array.slice();
+const setRandomPosts = (array) => _.shuffle(array).slice(0, RANDOM_PICTURES_QUANTITY);
+const setCommentsPosts = (array) => array.slice().sort((prev, next) => next.comments.length - prev.comments.length);
+
+const updateFilter = (array) => {
+
+  const filterTypes = {
+    'filter-default': setDefaultPosts(array),
+    'filter-random': setRandomPosts(array),
+    'filter-discussed': setCommentsPosts(array),
+  }
+
+  let filterType = filterTypes[imgFilter.querySelector('.img-filters__button--active').id];
+  return filterType;
+}
+
+const updatePictures = () => {
+  document.querySelectorAll('.picture').forEach(element => element.remove());
+}
+
+const onFilterClick = (evt, posts) => {
+  if (!evt.target.classList.contains('img-filters__button')) {
+    return;
+  } else {
+    let activeElement = evt.target.parentElement.querySelector('.img-filters__button--active');
+    if (activeElement) {
+      activeElement.classList.remove('img-filters__button--active');
+    }
+    evt.target.classList.add('img-filters__button--active');
+    const debounceRender = _.debounce(drawUsersPictures, BIDE);
+    debounceRender(updateFilter(posts));
+  }
+}
+
+export { imgFilter, imgFilterForm, updatePictures, onFilterClick }
--- js/util.js
+++ js/util.js
@@ -1,42 +1,4 @@
-
-//Основу брал из источника: https://learn.javascript.ru/task/random-int-min-max
-
-// Функция проверки макс меньше либо равен мин
-const isMaxLessThanMin = (min, max) => (max <= min);
-
-// Функция проверки мин и макс меньше нуля
-const isMinMaxLessThanZero = (min, max) => (min < 0) || (max < 0);
-
-// Функция вывода рандоного числа в заданном промежутке
-const randomInteger = function (min, max) {
-  const rand = min + Math.random() * (max + 1 - min);
-  if (isMinMaxLessThanZero(min, max) || isMaxLessThanMin(min, max)) {
-    window.console.log('invalid value');
-  } else {
-    return Math.floor(rand);
-  }
-}
-
-//Функция для проверки максимальной длины строки. получаем либо true либо false
-const countingSymbols = function (text, maxValue) {
-  return text.length <= maxValue;
-}
-window.console.log(countingSymbols('Привет JS', 9));
-
-//Случайное уникальное число
-const uniqNumbers = [];
-const getRandUniqNumbers = function () {
-  let isUniq = false;
-  while (isUniq == false) {
-    const rand = randomInteger(1, 1000);
-    if (!uniqNumbers.includes(rand)) {
-      uniqNumbers.push(rand);
-      isUniq = true;
-    }
-  }
-  return uniqNumbers[uniqNumbers.length - 1];
-};
-
+//Всплывающие сообщение об ошибке, но можно так и об успехе)
 const ALERT_SHOW_TIME = 5000;
 
 const showAlert = (message) => {
@@ -64,4 +26,4 @@ const isEscape = (evt) => {
   return evt.key === 'Escape' || evt.key === 'Esc';
 };
 
-export { randomInteger, getRandUniqNumbers, showAlert, isEscape };
+export { showAlert, isEscape };
